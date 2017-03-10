5f91278f5976d47ce9e49a7ed5df9143de6724b2
Jason Andrada <jpa323@nyu.edu>
2017-03-10 10:56:15 -0500

    fixed two issues, ran into three more

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,12 +1,23 @@
 $(document).ready(function () {
-    const button = $('#test');
+    const form = $('.find-cards');
     // this works. API is live.
     // AJAX calls that return more than one card need to be looped through
-    button.click(function () {
+    const urlAddOns = [];
+    const addOn = function (text) {
+        if (text != undefined) {
+            urlAddOns.push(text);
+        }
+    }
+    addOn($('.name-input').val());
+    addOn($('.color-input').val());
+    addOn($('.type-input').val());
+    form.on('submit', function (e) {
+        e.preventDefault();
         $.ajax({
-            url: 'https://api.deckbrew.com/mtg/cards?name=jace'
+            url: 'https://api.deckbrew.com/mtg/cards?'
             , type: 'GET'
             , success: function (data) {
+                $('#card-show').empty();
                 loop(data);
             }
             , error: function (error) {
@@ -17,36 +28,20 @@ $(document).ready(function () {
     //use this to append everything
     //also remember to add a CSS class so that this shit looks pretty, please.
     const loop = function (array) {
-        //instead of console logs, though, generate divs and shit. you know what to do.
-        array.forEach(function (thing) {
-            let testName = $('<div>');
-            testName.text(thing.name);
-            testName.addClass('testClass')
-            $('body').append(testName);
-            let testImg = $('<img>');
-            testImg.attr('src', thing.editions[0].image_url);
-            $('body').append(testImg);
-        });
-    };
-    /*taking out duplicates: 
-    const uniqueCards = [];
-    if ($.inArray(thing.id, uniqueCards) === -1){
-        uniqueCards.push(thing);
-    }
-    uniqueCards.forEach(function(thing))
-    */
-    /* double faced cards are gonna be an issue
-     if(thing.layout ==='double-faced'){
-     the other side has its own entry
-     $.ajax({
-     url: mtg api/ thing.names[1],
-     type: 'GET',
-     success: function(data){
-     put something that changes imageUrl on testImg
-     })
-     }
-     })
-    }
-    function hoverClass(image){
-    */
+            //        instead of console logs, though, generate divs and shit. you know what to do.
+            array.forEach(function (thing) {
+                    let testName = $('<div>');
+                    testName.text(thing.name);
+                    testName.addClass('testClass')
+                    $('#card-show').append(testName);
+                    let testImg = $('<img>');
+                    if (thing.editions[0].image_url.indexOf('https://image.deckbrew.com/mtg/multiverseid/0.jpg') != -1) {
+                        testImg.attr('src', thing.editions[1].image_url)
+                    }
+                    else {
+                        testImg.attr('src', thing.editions[0].image_url);
+                    }
+                    $('#card-show').append(testImg);
+                }) //end of forEach method
+        } //end of loop function
 }); //end of document.ready
